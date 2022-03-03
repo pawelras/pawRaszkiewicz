aed2e657960cf0126474f31e474d58f71294dab2
Pawel Raszkiewicz <pawraszkiewicz@gmail.com>
2022-03-03 17:15:11 +0000

    country borders

--- project1/libs/js/script.js
+++ project1/libs/js/script.js
@@ -1,37 +1,151 @@
-let map = L.map('map').locate({setView: true, maxZoom: 6});
+//Creating a map
 
+let map = L.map('map').locate({setView: true, maxZoom: 12});
 
-L.tileLayer('https://api.maptiler.com/maps/hybrid/{z}/{x}/{y}.jpg?key=X3eTl1pQfAaR1PxRqddg', {
-	attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
-	maxZoom: 6
-}).addTo(map);;
+// L.tileLayer('https://api.maptiler.com/maps/hybrid/{z}/{x}/{y}.jpg?key=X3eTl1pQfAaR1PxRqddg', {
+// 	attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+// 	maxZoom: 6
+// }).addTo(map);
+
+var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 8,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// Getting user's location
+
+function getLocation() {
+	if (navigator.geolocation) {
+	  navigator.geolocation.getCurrentPosition(success, fail);
+	} else {
+	  console.log("Geolocation is not supported by this browser.");
+	}
+	function success(position) {
+		let latitude = position.coords.latitude;
+		let longitude = position.coords.longitude;
+		console.log(latitude, longitude);
+	  }
+	
+	  function fail()
+		 {
+		  Console.log("Could not obtain location");
+		 }
+  }
+  
+  
+
+// Fetching Countries List
 
 $.ajax({
 	url: 'libs/php/getCountries.php',
 	type: 'GET',
+	dataType: 'json',
 	
 	success: function(result) {
+            
+		if (result.status.name == "ok") {
 
-		console.log(result);
 		
-		// if (result.status.name == "ok") {
-
-		// 	if (result.data.length === 0) { 
-		// 		console.log("No results")
-
-		// 	} else {
-
+			
+			for (const property in result.data) {
+				
+				
+				$('#countryList').append($('<option></option>').val(result.data[property]).text(property)); 
+			}
 
 			
-			// const countries = result.data.map(element => '<li><h3>' + element.title + '</h3><img src="' + element.thumbnailImg + '"/><p>' + element.summary + '</li>');
-			// $("#wikiResults").html('<ol>' + countries.join('') + '</ol>');
 			
-			
-		// 	}
-		// }
+		}
 	
 	},
 	error: function(jqXHR, textStatus, errorThrown) {
-		// your error code
+		console.log(errorThrown);
 	}
 }); 
+
+//Fetching Country info from API
+
+$('#countryList').on('change', function getCountryInfo() {
+	console.log($(this).val());
+	$.ajax({
+		url: 'libs/php/GetCountryInfo.php',
+		type: 'POST',
+		dataType: 'json',
+		data: {
+			countryISO: $('#countryList').val()
+		},
+
+		success: function(result) {
+
+			
+			if (result.status.name == "ok") {
+	
+			renderCountryDetails(result);
+			
+				
+				
+			}
+		
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			console.log(errorThrown);
+		}
+
+	})
+})
+
+function renderCountryDetails(result) {
+
+	$('#country-name-header').html($('#countryList option:selected').text());
+	$('#area').html(result.data[0].areaInSqKm);
+	$('#capital').html(result.data[0].capital);
+	$('#languages').html(result.data[0].languages);
+	$('#population').html(result.data[0].population);
+	$('#continent').html(result.data[0].continent);
+
+}
+
+//Getting and rendering country borders
+
+$('#countryList').on('change', function getCountryBorders() {
+
+	$.ajax({
+		url: 'libs/php/getCountryBorders.php',
+		type: 'POST',
+		dataType: 'json',
+		data: {
+			countryISO: $('#countryList').val()
+		},
+
+
+		success: function(result) {
+			
+			if (result.status.name == "ok") {
+			console.log(result.data);
+			let coordinates = Object.values(result.data.features)[0].geometry.coordinates; //removes country number key
+			
+			console.log(coordinates);
+			renderCountryBorders(coordinates);
+						
+			}
+		
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			console.log(errorThrown);
+		}
+	})
+
+})
+
+let polygon;
+
+function renderCountryBorders(coordinates) {
+	console.log(coordinates);
+
+	
+	if (polygon) {
+		polygon.remove();
+	}
+	polygon = L.polygon(coordinates, {color:'red'}).addTo(map);
+	
+}
--- project1/libs/php/getCountryBorders.php
+++ project1/libs/php/getCountryBorders.php
@@ -1,27 +1,27 @@
 <?php
 
+
     ini_set('display_errors', 'On');
     error_reporting(E_ALL);
 
     $executionStartTime = microtime(true) / 1000;
 
     $decode = json_decode(file_get_contents('../../geoData/countryBorders.geo.json'), true);
+    $countries_array = $decode["features"];
+	
+   function getBorderCoordinates($element) {
+       return $element["properties"]["iso_a2"] == $_REQUEST['countryISO'];
 
-	$countries_array = $decode["features"];
-    
+  }
 
-    foreach($countries_array as $element) {
-        $key = $element["properties"]["name"];
-        $value = $element["properties"]["iso_a2"];
-        $result_array[$key] = $value;
-
-    }
+  $data = array_filter($countries_array, "getBorderCoordinates");
+  $decode["features"] = $data;
     
     $output['status']['code'] = "200";
     $output['status']['name'] = "ok";
     $output['status']['description'] = "success";
     $output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
-    $output['data'] = $result_array;
+    $output['data'] = $decode;
 
     header('Content-Type: application/json; charset=UTF-8');
 
@@ -30,4 +30,3 @@
     echo json_encode($output);
 
 ?>
-
